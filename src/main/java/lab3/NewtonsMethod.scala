1eb271bf91e92806d96491697a4078956925ff23
Kirils_Savinihs <kirills111@gmail.com>
2018-10-09 16:30:51 +0300

    lab3

---
+++ src/main/java/lab3/NewtonsMethod.scala
@@ -0,0 +1,14 @@
+object NewtonsMethod {
+  def main(args: Array[String]): Unit = {
+    Console.println("sqrt(42):" + sqrt(42))
+    Console.println("sqrt(42)^2" + Math.pow(sqrt(42), 2))
+  }
+
+  def sqrt(x: Double): Double = sqrtIter(1, x)
+
+  def isGoodEnough(y: Double, x: Double): Boolean = Math.abs(Math.pow(y, 2) - x) < 0.001
+
+  def improve(y: Double, x: Double): Double = (y + x / y) / 2
+
+  def sqrtIter(y: Double, x: Double): Double = if (isGoodEnough(y, x)) y else sqrtIter(improve(y, x), x)
+}
